c355f697d3cd23aa2de6531a7d6f5c5b24ffe48b
Ashad Naushad <mohda170602@gmail.com>
2024-02-24 18:12:18 +0530

    ADDED MAP

--- app/src/main/java/com/example/flomate/ui/fragments/dashboard/ChatBotFragment.kt
+++ app/src/main/java/com/example/flomate/ui/fragments/dashboard/ChatBotFragment.kt
@@ -7,9 +7,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebSettings
 import com.example.flomate.databinding.ChatBotFragmentBinding
 import com.example.flomate.ui.fragments.BaseFragment
+import com.example.flomate.utils.Constants.CHAT_BOT
 
 class ChatBotFragment : BaseFragment() {
     private val binding by lazy { ChatBotFragmentBinding.inflate(layoutInflater) }
@@ -24,12 +24,12 @@ class ChatBotFragment : BaseFragment() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.startchat.setOnClickListener {
-            openUrl("https://ameenurrehman.github.io/Valentina/")
+            openUrl()
         }
     }
 
-    private fun openUrl(link: String) {
-        val uri = Uri.parse(link)
+    private fun openUrl() {
+        val uri = Uri.parse(CHAT_BOT)
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
     }
--- app/src/main/java/com/example/flomate/ui/fragments/dashboard/MapFragment.kt
+++ app/src/main/java/com/example/flomate/ui/fragments/dashboard/MapFragment.kt
@@ -6,14 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.flomate.databinding.MapFragmentBinding
 import com.example.flomate.ui.fragments.BaseFragment
+import com.example.flomate.utils.Constants.MAP_URL
 
-class MapFragment:BaseFragment() {
+class MapFragment : BaseFragment() {
     private val binding by lazy { MapFragmentBinding.inflate(layoutInflater) }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl(MAP_URL)
+
     }
 }
